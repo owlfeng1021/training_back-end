794d179774bf59330a92490c68aa0298ad334370
owlfeng1021 <1033571362@qq.com>
2019-06-20 21:14:18 +0800

    springSecurity oauth2 jwt 基本框架建成

---
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/config/ResourceServerConfig.java
@@ -0,0 +1,10 @@
+package com.xuecheng.manage_course.config;
+
+/**
+ * @ClassName ResourceServerConfig
+ * @Author: owlfeng
+ * @Description: TODO
+ * @Date: Create in 16:24 2019/6/20
+ */
+public class ResourceServerConfig {
+}
